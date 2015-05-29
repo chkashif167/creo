2aa05d310aeec5830afdd4ff09e480bd7e3f2bbe
Anam Imtiaz <anam.imtiaz01@gmail.com>
2015-05-29 15:26:04 +0500

    Add slider and featured category block

--- app/code/community/Mks/Responsivebannerslider/Block/Adminhtml/Responsivebannersliderbackend.php
+++
@@ -1,5 +0,0 @@
-<?php  
-
-class Mks_Responsivebannerslider_Block_Adminhtml_Responsivebannersliderbackend extends Mage_Adminhtml_Block_Template {
-
-}
--- app/code/community/Mks/Responsivebannerslider/Helper/Data.php
+++
@@ -1,5 +0,0 @@
-<?php
-class Mks_Responsivebannerslider_Helper_Data extends Mage_Core_Helper_Abstract
-{
-}
-	 
