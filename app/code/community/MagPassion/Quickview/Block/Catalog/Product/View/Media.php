9c1977063558a92902b1168043ae2e3c520307f3
Anam Imtiaz <anam.imtiaz01@gmail.com>
2015-06-11 10:18:59 +0500

    add new files

---
+++ app/code/community/MagPassion/Quickview/Block/Catalog/Product/View/Media.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * MagPassion_Quickview extension
+ * 
+ * @category   	MagPassion
+ * @package		MagPassion_Quickview
+ * @copyright  	Copyright (c) 2014 by MagPassion (http://magpassion.com)
+ * @license	http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class MagPassion_Quickview_Block_Catalog_Product_View_Media extends Mage_Catalog_Block_Product_View_Media
+{
+}
---
+++ app/code/community/MagPassion/Quickview/controllers/IndexController.php
@@ -0,0 +1,132 @@
+<?php
+
+/**
+ * MagPassion_Quickview extension
+ * 
+ * @category   	MagPassion
+ * @package		MagPassion_Quickview
+ * @copyright  	Copyright (c) 2014 by MagPassion (http://magpassion.com)
+ * @license	http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+
+class MagPassion_Quickview_IndexController extends Mage_Core_Controller_Front_Action
+{
+    /**
+     * Initialize requested product object
+     *
+     * @return Mage_Catalog_Model_Product
+     */
+    protected function _initProduct()
+    {
+        Mage::dispatchEvent('catalog_controller_product_init_before', array('controller_action'=>$this));
+        $productId = NULL;
+        if (isset($_GET['pid']) && is_numeric($_GET['pid']))
+            $productId  = (int) $_GET['pid'];
+		
+        if (!$productId) {
+            return false;
+        }
+
+        $product = Mage::getModel('catalog/product')
+            ->setStoreId(Mage::app()->getStore()->getId())
+            ->load($productId);
+
+        if (!Mage::helper('catalog/product')->canShow($product)) {
+            return false;
+        }
+        if (!in_array(Mage::app()->getStore()->getWebsiteId(), $product->getWebsiteIds())) {
+            return false;
+        }
+        
+        Mage::register('current_product', $product);
+        Mage::register('product', $product);
+
+        try {
+            Mage::dispatchEvent('catalog_controller_product_init', array('product'=>$product));
+            Mage::dispatchEvent('catalog_controller_product_init_after', array('product'=>$product, 'controller_action' => $this));
+        } catch (Mage_Core_Exception $e) {
+            Mage::logException($e);
+            return false;
+        }
+
+        return $product;
+    }
+
+    /**
+     * Initialize product view layout
+     *
+     * @param   Mage_Catalog_Model_Product $product
+     * @return  Mage_Catalog_ProductController
+     */
+    protected function _initProductLayout($product)
+    {
+        $update = $this->getLayout()->getUpdate();
+        $update->addHandle('default');
+        $this->addActionLayoutHandles();
+
+        $update->addHandle('PRODUCT_TYPE_'.$product->getTypeId());
+        $update->addHandle('PRODUCT_'.$product->getId());
+
+        if ($product->getPageLayout()) {
+            $this->getLayout()->helper('page/layout')
+                ->applyHandle($product->getPageLayout());
+        }
+
+        $this->loadLayoutUpdates();
+
+
+        $update->addUpdate($product->getCustomLayoutUpdate());
+
+        $this->generateLayoutXml()->generateLayoutBlocks();
+
+        if ($product->getPageLayout()) {
+            $this->getLayout()->helper('page/layout')
+                ->applyTemplate($product->getPageLayout());
+        }
+
+        //$currentCategory = Mage::registry('current_category');
+        if ($root = $this->getLayout()->getBlock('root')) {
+            $root->addBodyClass('product-'.$product->getUrlKey());
+            
+        }
+        return $this;
+    }
+
+    /**
+     * View product action
+     */
+    public function viewAction()
+    {
+        if ($product = $this->_initProduct()) {
+            Mage::dispatchEvent('catalog_controller_product_view', array('product'=>$product));
+
+            if ($this->getRequest()->getParam('options')) {
+                $notice = $product->getTypeInstance(true)->getSpecifyOptionMessage();
+                Mage::getSingleton('catalog/session')->addNotice($notice);
+            }
+
+            Mage::getSingleton('catalog/session')->setLastViewedProductId($product->getId());
+            Mage::getModel('catalog/design')->applyDesign($product, Mage_Catalog_Model_Design::APPLY_FOR_PRODUCT);
+
+            $this->_initProductLayout($product);
+            $this->_initLayoutMessages('catalog/session');
+            $this->_initLayoutMessages('tag/session');
+            $this->_initLayoutMessages('checkout/session');
+            $headBlock = $this->getLayout()->getBlock('head');
+	        if ($headBlock) {
+	            
+	                $headBlock->setRobots('noindex,follow');
+	            
+	        }
+            $this->renderLayout();
+        }
+        else {
+            if (isset($_GET['store'])  && !$this->getResponse()->isRedirect()) {
+                $this->_redirect('');
+            } elseif (!$this->getResponse()->isRedirect()) {
+                $this->_forward('noRoute');
+            }
+        }
+    }
+}
