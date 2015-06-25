60ddc61b0c71db933e69d30bdf2db87c369760c7
chkashif167 <chkashif167@gmail.com>
2015-06-25 12:04:31 +0500

    checkoutpage completed

--- skin/frontend/creo/default/js/jquery.main.js
+++ skin/frontend/creo/default/js/jquery.main.js
@@ -996,17 +996,6 @@ function initTabs() {
 			$('header#header').removeClass('small-header');
 		}
 	});
-	jQuery('.up').on('click',function(){
-jQuery(this).next('#qty').val(parseInt(jQuery(this).next('#qty').val())+1);
-});
-
-
-jQuery('.down').on('click',function(){
-if(jQuery(this).prev('#qty').val() > 1) {
-jQuery(this).prev('#qty').val(parseInt(jQuery(this).prev('#qty').val())-1);
-}
-});
-
 });
 
 
@@ -1034,6 +1023,16 @@ $j(window).on('resize, load, ready', centerModals);
  */
 
 
+jQuery('.up').on('click',function(){
+jQuery(this).next('#qty').val(parseInt(jQuery(this).next('#qty').val())+1);
+});
+
+
+jQuery('.down').on('click',function(){
+if(jQuery(this).prev('#qty').val() > 1) {
+jQuery(this).prev('#qty').val(parseInt(jQuery(this).prev('#qty').val())-1);
+}
+});
 
 
  /*
