319dc18b8a525609815cf224b5505aee9734e551
Imran Zahoor <imranzahoor@bitbucket.org>
2015-08-22 00:07:37 +0500

    Proceed to checkout fix.

--- app/code/core/Mage/Core/Model/Session/Abstract/Varien.php
+++ app/code/core/Mage/Core/Model/Session/Abstract/Varien.php
@@ -115,7 +115,7 @@ class Mage_Core_Model_Session_Abstract_Varien extends Varien_Object
             $cookieParams['domain'] = $cookie->getDomain();
         }
 
-//        call_user_func_array('session_set_cookie_params', $cookieParams);
+        call_user_func_array('session_set_cookie_params', $cookieParams);
 
         if (!empty($sessionName)) {
             $this->setSessionName($sessionName);
