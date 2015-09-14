123d46efda30ed7583e1bf2134fe61f781f59d36
chkashif167 <chkashif167@gmail.com>
2015-09-14 21:11:37 +0500

    mobile detect plugin

---
+++ app/code/community/Optimiseweb/MobileDetect/Helper/Data.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Optimiseweb MobileDetect Helper Data
+ *
+ * @package     Optimiseweb_MobileDetect
+ * @author      Kathir Vel (vkathirvel@gmail.com)
+ * @copyright   Copyright (c) 2015 Kathir Vel
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+require_once Mage::getModuleDir('', 'Optimiseweb_MobileDetect') . DS . 'Library' . DS . 'Mobile_Detect.php';
+
+class Optimiseweb_MobileDetect_Helper_Data extends Mobile_Detect
+{
+    
+}
