4c2b4ba9ef7a496e1c860ed6e356bbeb56bfd851
Kashif <Kashif>
2015-12-22 18:49:23 +0500

    cart page, responsive table,

--- skin/frontend/creo_mobile/default/js/jquery.main.js
+++ skin/frontend/creo_mobile/default/js/jquery.main.js
@@ -2686,25 +2686,13 @@ jQuery(window).load(function(){
 /** Selection of the first style ends here **/
 
 
-jQuery(document).ready(function($){
+/*jQuery(document).ready(function($){
     jQuery(".bgr-icon").click(function() {
        var id = jQuery(this).attr('data-id');
        jQuery("#" + id).slideToggle();
     });
     jQuery('.data-table,.order-products-table,.cart-table-area').stacktable();
-});
+});*/
 
 
-jQuery(document).ready(function() { 
-    //pure javascript
 
-    var pathname = window.location.href;
-    if (pathname.indexOf("universal_categories") > 0){
-		jQuery(".fme_layered_clearall").removeClass("progos");
-    }
-	else
-	{
-		jQuery(".fme_layered_clearall").addClass("progos");
-	}
-    
-});
