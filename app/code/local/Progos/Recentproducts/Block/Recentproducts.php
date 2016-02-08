9083b62659b6ac221b13d91b286cc41641c47737
fahimktk <danish.ktk@gmail.com>
2016-02-08 16:55:28 +0500

    recent products

--- app/code/local/Progos/Recentproducts/Block/Recentproducts.php
+++ app/code/local/Progos/Recentproducts/Block/Recentproducts.php
@@ -1,19 +1,21 @@
 <?php
 class Progos_Recentproducts_Block_Recentproducts extends Mage_Core_Block_Template {
-  public function getRecentProducts() {
+  public function getRecentProducts($cat_to_select,$pcount) {
     // call model to fetch data
+
     $arr_products = array();
-    $products = Mage::getModel("recentproducts/recentproducts")->getRecentProducts();
+    $products = Mage::getModel("recentproducts/recentproducts")->getRecentProducts($cat_to_select,$pcount);
   
     foreach ($products as $product) {
       $arr_products[] = array(
         'id' => $product->getId(),
         'name' => $product->getName(),
         'url' => $product->getProductUrl(),
-        'image' => $product->getProductImageUrl(),
+        'image' => $product->getImage(),
+        'thumb' => $product->getSmallImage(),
       );
     }
-  
+ 
     return $arr_products;
   }
 }
--- app/code/local/Progos/Recentproducts/Model/Recentproducts.php
+++ app/code/local/Progos/Recentproducts/Model/Recentproducts.php
@@ -1,11 +1,15 @@
 <?php
 class Progos_Recentproducts_Model_Recentproducts extends Mage_Core_Model_Abstract {
-  public function getRecentProducts() {
+  public function getRecentProducts($cat_to_select,$pcount) {
+    
     $products = Mage::getModel("catalog/product")
                 ->getCollection()
+                ->joinField('category_id', 'catalog/category_product', 'category_id', 'product_id = entity_id', null, 'left')
                 ->addAttributeToSelect('*')
                 ->setOrder('entity_id', 'DESC')
-                ->setPageSize(6);
+                ->addAttributeToFilter('category_id', array('in' => $cat_to_select))
+                ->setPageSize($pcount);
+
     return $products;
   }
 }
