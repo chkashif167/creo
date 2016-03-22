dff0909684d17e09e04cc566549dcd8cf4819db9
nzsiddiqui <nzsiddiqui@gmail.com>
2016-03-22 15:45:46 +0500

    creo ar js validation text domain for login page

--- js/prototype/validation.js
+++ js/prototype/validation.js
@@ -528,11 +528,11 @@ Validation.addAllThese([
                 //return Validation.get('IsEmpty').test(v) || /^[\!\#$%\*/?|\^\{\}`~&\'\+\-=_a-z0-9][\!\#$%\*/?|\^\{\}`~&\'\+\-=_a-z0-9\.]{1,30}[\!\#$%\*/?|\^\{\}`~&\'\+\-=_a-z0-9]@([a-z0-9_-]{1,30}\.){1,5}[a-z]{2,4}$/i.test(v)
                 return Validation.get('IsEmpty').test(v) || /^([a-z0-9,!\#\$%&'\*\+\/=\?\^_`\{\|\}~-]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z0-9,!\#\$%&'\*\+\/=\?\^_`\{\|\}~-]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*@([a-z0-9-]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z0-9-]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*\.(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]){2,})$/i.test(v)
             }],
-    ['validate-emailAdress', 'Email Adress is a required field.', function(v) {
+    ['validate-emailAdress', errors_arr['validate-emailAdress'], function(v) {
             return !Validation.get('IsEmpty').test(v);
         }],
 
-    ['validate-passwordlogin', 'Password is a required field.', function(v) {
+    ['validate-passwordlogin', errors_arr['validate-passwordlogin'], function(v) {
             return !Validation.get('IsEmpty').test(v);
         }],
 
